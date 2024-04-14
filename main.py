e1f6e42ec5114ccaa68d865843daab0232a5a16c
animus-dolum <75898444+animus-dolum@users.noreply.github.com>
2024-04-14 22:54:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    with open('templates/index.html', 'r', encoding='utf-8') as stream:
+        return stream.read()
+
+
+@app.route('/t')
+def t():
+    with open('t.html', 'r', encoding='utf-8') as stream:
+        return stream.read()
+
+
+@app.route('/reg_form', methods=['POST', 'GET'])
+def reg_form():
+    if request.method == 'GET':
+        return render_template('reg_form.html')
+    elif request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        print(request.form['class'])
+        print(request.form['file'])
+        print(request.form['about'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        return "Форма отправлена"
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
