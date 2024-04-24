f9f7b7388f80f38be9f8cd55136a1d2cc3dcc522
animus-dolum <75898444+animus-dolum@users.noreply.github.com>
2024-04-25 00:09:55 +0300

    Update main.py
    /фокина
    
    форма регистрации, отправка данных в бд
    логин пока не работает

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response, jsonify, redirect
+from flask_restful import reqparse, abort, Api, Resource
+from data import db_session, weather_api
+from data.users import User
+from forms.forms import RegisterForm, LoginForm
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_restful import reqparse, abort, Api, Resource
 
-icons = ["bi bi-cloud-snow-fill", "bi bi-cloud-drizzle-fill", "bi bi-cloud-fog-fill", "bi bi-cloud-lightning-rain-fill",
-         "bi bi-cloud-moon-fill", "bi bi-cloud-sun-fill", "bi bi-sun-fill", "bi bi-moon-fill", "bi bi-snow3", "bi bi-sun-fill"]
-icons_day = []
-icon_paths = []
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 
-def icons_day():
-    global icons
-    global icons_day
-    for i in icons:
-        icons_day.append(i)
-
-def icons_week():
-    pass
-
-def temps_day():
-    pass
-
-def temps_week():
-    pass
 
 @app.route('/')
 @app.route('/index')
@@ -29,39 +20,60 @@ def index():
         return stream.read()
 
 
-@app.route('/t')
-def t():
-    with open('t.html', 'r', encoding='utf-8') as stream:
-        return stream.read()
-
-@app.route('/main_page')
-def main_page():
-    temp_day = 50
-    temps_day = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]#temps_day()
-    temps_week = [0, 1, 2, 3, 4, 5, 6]#temps_week()
-    home = 'Москва'
-    humidity = 60
-    rainfall = 0
-    speed = 20
-    return render_template('main_page.html', temps_day=temps_day,
-                           temp_day=temp_day, home=home, humidity=humidity, rainfall=rainfall, speed=speed,
-                           icons_day=icons_day, temps_week=temps_week)
-
-
 @app.route('/reg_form', methods=['POST', 'GET'])
 def reg_form():
-    if request.method == 'GET':
-        return render_template('reg_form.html')
-    elif request.method == 'POST':
-        print(request.form['email'])
-        print(request.form['password'])
-        print(request.form['name'])
-        print(request.form['surname'])
-        print(request.form['country'])
-        print(request.form['home'])
-        print(request.form['sex'])
-        return "Форма отправлена"
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            email=form.email.data,
+            country=form.country.data,
+            city=form.city.data,
+            sex=form.sex.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main():
+    db_session.global_init("db/info.db")
+    app.run(host='127.0.0.1', port=8080)
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+    main()
