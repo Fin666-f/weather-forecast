ed6800624b4a28867c2a3ea8dff4bd470d29136f
user <alexandrafuntikova4833@gmail.com>
2024-04-24 19:17:24 +0300

    Главная страница 5(Фунтикова)

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from flask import Flask, request, render_template
 
+icons = ["bi bi-cloud-snow-fill", "bi bi-cloud-drizzle-fill", "bi bi-cloud-fog-fill", "bi bi-cloud-lightning-rain-fill",
+         "bi bi-cloud-moon-fill", "bi bi-cloud-sun-fill", "bi bi-sun-fill", "bi bi-moon-fill", "bi bi-snow3", "bi bi-sun-fill"]
+icons_day = []
+icon_paths = []
 app = Flask(__name__)
 
+
+def icons_day():
+    global icons
+    global icons_day
+    for i in icons:
+        icons_day.append(i)
+
+def icons_week():
+    pass
+
+def temps_day():
+    pass
+
+def temps_week():
+    pass
 
 @app.route('/')
 @app.route('/index')
@@ -17,7 +36,16 @@ def t():
 
 @app.route('/main_page')
 def main_page():
-    return render_template('main_page.html')
+    temp_day = 50
+    temps_day = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]#temps_day()
+    temps_week = [0, 1, 2, 3, 4, 5, 6]#temps_week()
+    home = 'Москва'
+    humidity = 60
+    rainfall = 0
+    speed = 20
+    return render_template('main_page.html', temps_day=temps_day,
+                           temp_day=temp_day, home=home, humidity=humidity, rainfall=rainfall, speed=speed,
+                           icons_day=icons_day, temps_week=temps_week)
 
 
 @app.route('/reg_form', methods=['POST', 'GET'])
